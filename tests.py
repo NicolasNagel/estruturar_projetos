3d2750cfd95bf366a54bd3ac752c21848b41e170
Nicolas <nicolascnagel@gmail.com>
2025-08-21 20:35:24 -0300

    adicionado mais uma rota

--- tests.py
+++ tests.py
@@ -18,6 +18,15 @@ def test_listar_produtos_status_code():
     response = client.get("/produtos")
     assert response.status_code == 200
 
-def test_listar_produtos_json():
+def test_tamanho_list_produtos():
     response = client.get("/produtos")
     assert len(response.json()) == 3
+
+def test_pega_um_produto():
+    response = client.get("/produtos/1")
+    assert response.json() == {
+        "id": 1,
+        "nome": "Smartphone",
+        "descricao": "Um telefone que é inteligente",
+        "preco": 1500.0,
+    }
